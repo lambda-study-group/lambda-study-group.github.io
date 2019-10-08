7a34bfd0998310ab035c097e323fe6ad58f21821
Jeova Gomes <jeova.gomes@ngi.com.br>
2019-10-08 13:45:38 -0300

    chore: add initial styleguide module

---
+++ src/StyleGuide.elm
@@ -0,0 +1,20 @@
+module StyleGuide exposing (colors)
+
+import Css exposing (hex)
+
+
+colors =
+    { text = hex "ffffff"
+    , link = hex "e499f0"
+    , icon = hex "ffffff"
+    , background = hex "000000"
+    , combinator = hex "36223A"
+    , pink = hex "d872e9"
+    }
+
+
+
+-- # Todo
+-- Text sizes
+-- Breakpoints
+-- Spacings
