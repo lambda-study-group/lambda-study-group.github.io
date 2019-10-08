102bcf32ed8909cb0906b9923a9c14ed315e5770
Jeova Gomes <jeova.gomes@ngi.com.br>
2019-10-08 13:44:34 -0300

    chore: separate contents

---
+++ src/Contents.elm
@@ -0,0 +1,73 @@
+module Contents exposing (..)
+
+-- Todo
+-- Fix typos
+
+
+title =
+    "Lambda study group!"
+
+
+description =
+    """
+    O intuito do grupo é a participação coletiva de todos e o aprendizado de
+    cada um. No grupo do telegram você pode tirar duvidas, sugerir projetos e
+    conversar sobre temas relacionados a programação funcional Para praticar
+    temos alguns repositórios em que você pode resolver alguns exercicios.
+    """
+
+
+invite =
+    "Entre no nosso grupo do "
+
+
+telegramLinkData =
+    [ "https://t.me/lambdastudygroup", "telegram" ]
+
+
+projects =
+    { title = "Projetos Atuais"
+    , content =
+        [ [ "https://github.com/lambda-study-group/telegram-bot"
+          , "Telegram Bot"
+          ]
+        , [ "https://github.com/lambda-study-group/awesome-functional-studies"
+          , "Juntar conteudo academico e pratico de várias linguagens em um awesome"
+          ]
+        , [ "https://github.com/lambda-study-group/dicionario-termos-funcionais"
+          , "Dicionario de tradução de termos funcionais"
+          ]
+        , [ "https://github.com/lava-jato-the-game/lava_jato"
+          , "Desenvolvimento do jogo da Lava Jato com Phoenix(Absinthe) / ClojureScript"
+          ]
+        ]
+    }
+
+
+exercices =
+    { title = "Resolução de Exercicios"
+    , content =
+        [ [ "https://github.com/lambda-study-group/desafios"
+          , "Desafios"
+          ]
+        , [ "https://github.com/lambda-study-group/sicp"
+          , "Exercicios do livro SICP em várias linguagens diferentes"
+          ]
+        , [ "https://github.com/lambda-study-group/hacker-rank"
+          , "Exercicios da trilha de programação funcional do HackerRank"
+          ]
+        , [ "https://github.com/lambda-study-group/exercism"
+          , "Exercicios de linguagens funcionais no Exercism"
+          ]
+        ]
+    }
+
+
+tranlations =
+    { title = "Traduções"
+    , content =
+        [ [ "https://github.com/lambda-study-group/Functional-Light-JS"
+          , "Tradução do Functional Light Programming"
+          ]
+        ]
+    }
