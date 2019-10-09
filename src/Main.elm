7c684994a7a2a4906ed1362c16c012577922e835
jeovazero <jeovapereira2000@live.com>
2019-10-09 01:17:56 -0300

    chore(main): add the initial site structure

--- src/Main.elm
+++ src/Main.elm
@@ -1,58 +1,89 @@
 module Main exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Contents
+import Css exposing (..)
+import Css.Global exposing (body, global)
+import Elements exposing (contentWrapper, headerTitle, lambdaLogo, textStyled)
+import Html
+import Html.Styled exposing (..)
+import Html.Styled.Attributes as Attr exposing (..)
+import StyleGuide as Theme
+import Styles exposing (textLarge)
 
 
-view model =
-    div [] [
-        div [ class "navbar navbar-inverse navbar-fixed-top"] [
-            div [ class "container-fluid" ] [
-                div [ class "navbar-header" ] [
-                    a [ class "navbar-brand", href "#" ] [
-                        img [
-                            src "assets/logo.png"
-                            , width 20
-                            , alt "Logo do grupao"
-                        ] [text "Logo"]]
-                ]
+globalCss =
+    global
+        [ body
+            [ padding (px 0)
+            , margin (px 0)
+            , backgroundColor Theme.colors.background
             ]
-        ],
-        div [ class "jumbotron" ]
-            [ h1 [] [ text "Lambda study group!" ]
-            , p []
-                [ text "O intuito do grupo é a participação coletiva de todos e o aprendizado de cada um. No grupo do telegram você pode tirar duvidas, sugerir projetos e conversar sobre temas relacionados a programação funcional Para praticar temos alguns repositórios em que você pode resolver alguns exercicios."]
-            , p []
-                [ text "Entre no nosso grupo do  "
-                , a [href "https://t.me/lambdastudygroup" ] [strong [] [text "telegram"]]
-                , text "."]
-        ],
-        div [ class "project-box"]
-            [ div [] 
-                [ h4 [] [text "Projetos Atuais 💡"]
-                , ul [ class "list-group" ] 
-                    [ li [class "list-group-item"] [a [href "https://github.com/lambda-study-group/telegram-bot" ] [strong [] [text "Telegram Bot"]]]
-                    , li [class "list-group-item"] [a [href "https://github.com/lambda-study-group/awesome-functional-studies" ] [strong [] [text "Juntar conteudo academico e pratico de várias linguagens em um awesome"]]]
-                    , li [class "list-group-item"] [a [href "https://github.com/lambda-study-group/dicionario-termos-funcionais" ] [strong [] [text "Dicionario de tradução de termos funcionais"]]]
-                    , li [class "list-group-item"] [a [href "https://github.com/lava-jato-the-game/lava_jato" ] [strong [] [text "Desenvolvimento do jogo da Lava Jato com Phoenix(Absinthe) / ClojureScript"]]]
-                    ]
-                ]
-            , div [] 
-                [ h4 [] [text "Resolução de Exercicios"]
-                , ul [ class "list-group" ] 
-                    [ li [class "list-group-item"] [a [href "https://github.com/lambda-study-group/desafios" ] [strong [] [text "Desafios"]]]
-                    , li [class "list-group-item"] [a [href "https://github.com/lambda-study-group/sicp" ] [strong [] [text "Exercicios do livro SICP em várias linguagens diferentes"]]]
-                    , li [class "list-group-item"] [a [href "https://github.com/lambda-study-group/hacker-rank" ] [strong [] [text "Exercicios da trilha de programação funcional do HackerRank"]]]
-                    , li [class "list-group-item"] [a [href "https://github.com/lambda-study-group/exercism" ] [strong [] [text "Exercicios de linguagens funcionais no Exercism"]]]
-                ]]
-            , div []
-                [ h4 [] [text "Traduções"]
-                , ul [ class "list-group" ] 
-                [ li [class "list-group-item"] [a [href "https://github.com/lambda-study-group/Functional-Light-JS" ] [strong [] [text "Tradução do Functional Light Programming"]]]
-            ]]
         ]
-    ]
+
+
+lambdaTitle =
+    headerTitle Contents.title
+
+
+lambdaSeparator =
+    div
+        [ css
+            [ Css.width (px 120)
+            , backgroundColor Theme.colors.pink
+            , Css.height (px 4)
+            ]
+        ]
+        []
+
+
+lambdaDescription =
+    div [] []
+
+
+homeSection =
+    section
+        [ css
+            [ displayFlex
+            , justifyContent center
+            , flexDirection column
+            , alignItems center
+            , padding (rem 6)
+            , maxWidth (px 720)
+            , boxSizing borderBox
+            , margin auto
+            ]
+        ]
+        [ lambdaLogo
+        , lambdaTitle
+        , lambdaSeparator
+        , contentWrapper
+            [ textStyled [ css [ textLarge ] ] [ text Contents.description ]
+            ]
+        ]
+
+
+languagesSection =
+    section [] []
+
+
+repositoriesSection =
+    section [] []
+
+
+lambdaFooter =
+    footer [] []
+
+
+view =
+    div
+        []
+        [ globalCss
+        , homeSection
+        , languagesSection
+        , repositoriesSection
+        , lambdaFooter
+        ]
 
 
 main =
-    view "dummy model"
+    toUnstyled view
