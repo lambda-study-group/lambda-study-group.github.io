33f8067b52c5b375a3de42955c1e94ce0999febf
jeovazero <jeovapereira2000@live.com>
2019-10-09 01:15:46 -0300

    chore(styleguide): add spacing, fontfamily and textSize

--- src/StyleGuide.elm
+++ src/StyleGuide.elm
@@ -1,6 +1,6 @@
-module StyleGuide exposing (colors)
+module StyleGuide exposing (colors, fontFamily, spacing, textSize)
 
-import Css exposing (hex)
+import Css exposing (batch, fontFamilies, fontWeight, hex, normal, rem)
 
 
 colors =
@@ -13,8 +13,27 @@ colors =
     }
 
 
+textSize =
+    { small = rem 0.75
+    , medium = rem 1
+    , large = rem 2
+    , xLarge = rem 4
+    }
+
+
+spacing =
+    { tiny = rem 0.5
+    , small = rem 1
+    , medium = rem 2
+    , large = rem 3
+    , xLarge = rem 4
+    }
+
+
+fontFamily =
+    [ "Ropa Sans", "sans-serif" ]
+
+
 
 -- # Todo
--- Text sizes
 -- Breakpoints
--- Spacings
